84210ffc1f1e4aeac76e88d09c2a225a64932c6c
alencartha <alencar_tha@hotmail.com>
2022-10-17 16:46:58 -0300

    adiciona api web speech

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,27 @@
+const elChute = document.querySelector('#chute')
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result',(event)=>{
+    onSpeak(event)
+})
+
+
+function onSpeak(valor){
+    let chute = valor.results[0][0].transcript
+    exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela(chute){
+    elChute.innerHTML = 
+    `
+    <div>Você disse:</div>
+        <span class="box">${chute}</span>
+    <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+    
+    `
+}
