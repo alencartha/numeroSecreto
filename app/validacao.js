fd819d7c699f453b9ad6f9d5a773410eaf670336
alencartha <alencar_tha@hotmail.com>
2022-10-18 11:16:12 -0300

    validação acertou numero

--- app/validacao.js
+++ app/validacao.js
@@ -1,12 +1,20 @@
 function verificarSeOChutePossuiValorValido(chute) {
     const numero = +chute
 
-    if (chuteInvalido(chute)) {
-        console.log("Valor inválido")
+    if (chuteInvalido(numero)) {
+        elChute.innerHTML += `<div>Valor inválido</div>`
     }
 
-    if(numeroMaiorOuMenorQueOValorPermitido(numero)){
-        console.log(`Valor inválido. O número secreto precisa estar entre: ${menorValor} e ${maiorValor}`) 
+    if (numeroMaiorOuMenorQueOValorPermitido(numero)) {
+        elChute.innerHTML += ` <div>Valor inválido. O número secreto precisa estar entre: ${menorValor} e ${maiorValor}</div>`
+    }
+
+    if(numero === numeroSecreto){
+        document.body.innerHTML = 
+        `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto} </h3>
+        `
     }
 }
 
@@ -14,6 +22,6 @@ function chuteInvalido(numero) {
     return Number.isNaN(numero)
 }
 
-function numeroMaiorOuMenorQueOValorPermitido(numero){
+function numeroMaiorOuMenorQueOValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor
 }
