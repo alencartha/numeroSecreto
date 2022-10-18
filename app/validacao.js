5d6b595501d2233be696d200e876e7a2f8c3de5b
alencartha <alencar_tha@hotmail.com>
2022-10-18 11:31:38 -0300

    adiciona botao jogar novamente

--- app/validacao.js
+++ app/validacao.js
@@ -16,6 +16,9 @@ function verificarSeOChutePossuiValorValido(chute) {
             `
         <h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto} </h3>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente </button>
+
         `
     } else if (numero > numeroSecreto) {
         elChute.innerHTML += ` <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div> `
@@ -33,3 +36,9 @@ function chuteInvalido(numero) {
 function numeroMaiorOuMenorQueOValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e =>{
+    if(e.target.id == "jogar-novamente"){
+        window.location.reload()
+    }
+})
